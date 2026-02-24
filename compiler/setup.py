24a4a99764238d0788fc4141945435432f958a0a
LuxUmbris <luxumbris@icloud.com>
2026-02-24 14:46:09 +0000

    update setup.py

--- compiler/setup.py
+++ compiler/setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup, find_packages
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+root_dir = os.path.join(base_dir, "..")
+readme_path = os.path.join(root_dir, "README.md")
+
+with open(readme_path, encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="chiton-lang",
-    version="0.1.2",
+    version="0.1.3",
+    description="A high-performance systems programming language with Pythonic syntax and native C-ABI interoperability.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "llvmlite",
